60fe3496c1231c26d5a824baa900d367f4e26ebb
seolranlee <leesugar90@gmail.com>
2021-06-28 17:47:31 +0900

    404 페이지 처리: 2. 유효한 URL 정규식을 방문했지만 잘못된 품종을 입력했을 때 - render-props 사용하기

--- src/DogPage.js
+++ src/DogPage.js
@@ -1,29 +1,31 @@
-import { useState, useEffect } from 'react'
 import { get } from 'lodash'
 import { useParams, Link } from 'react-router-dom'
-import useQuery from './useQuery'
-import Page404 from './Page404'
+import Query from './Query'
 
 // 선택한 품종 강아지의 무작위 이미지를 보여주는 페이지. 품종은 URL 파라미터로 넘긴다.
 const DogPage = () => {
   const { breed } = useParams()
-  const { data, statusCode } = useQuery({
-    url: `https://dog.ceo/api/breed/${breed}/images/random`,
-  })
-
-  if(statusCode === 404) {
-    return <Page404 />
-  }
-
-  const imageSrc = get(data, 'message')
   return (
-    <div>
-      <div>
-        <Link to="/">back</Link>
-      </div>
-      {!imageSrc && <p>Loading...</p>}
-      {imageSrc && <img alt={`A nice ${breed}`} src={imageSrc} height={200} />}
-    </div>
+    <Query url={`https://dog.ceo/api/breed/${breed}/images/random`}>
+      {
+        ({ data }) => {
+          const imageSrc = get(data, 'message')
+          return (
+            <div>
+              <div>
+                <Link to="/">back</Link>
+              </div>
+              {!imageSrc && <p>Loading...</p>}
+              {imageSrc && (
+                <img alt={`A nice ${breed}`} src={imageSrc} height={200} />
+              )}
+            </div>
+          )
+        }
+      }
+
+    </Query>
+
   )
 }
 
---
+++ src/Query.js
@@ -0,0 +1,16 @@
+import Page404 from "./Page404";
+import useQuery from "./useQuery";
+
+const Query = ({ url, children }) => {
+  const { data, statusCode } = useQuery({ url })
+
+  if (statusCode === 404) {
+    return <Page404 />
+  }
+
+  // ... 등 여기서 여러 HTTP 응답 코드 처리
+
+  return children({data})
+}
+
+export default Query
